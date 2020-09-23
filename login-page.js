877909d0c97ecb3381fed8a068c56796625a6fc1
NathanFogwillRichuish <71495972+NathanFogwillRichuish@users.noreply.github.com>
2020-09-23 09:18:11 +0100

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -4,11 +4,11 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 
 function hideElement() {
   var x = document.getElementById("login-form");
-  var y =document.getElementById("button");
+  //var y =document.getElementById("button");
   if (x.style.display === "grid") {
     x.style.display = "none"
     //not working atm should display button when login-form is hidden but just hides the button as well
-    y.style.display = "grid";
+    //y.style.display = "grid";
   } else {
     x.style.display = "grid";
   }
